8191dbf8f08aa4cf733e393d25aa6c9ba38c7efa
MinnuSudhak <95743800+MinnuSudhak@users.noreply.github.com>
2022-01-07 15:13:16 +0530

    to show whether it is a leap year or not

---
+++ leap year.py
@@ -0,0 +1,13 @@
+def is_leap(year):
+   return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+year = int(input("enter the year: "))
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
